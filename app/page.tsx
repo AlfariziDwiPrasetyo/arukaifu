d8cfbfaf1bef7843b1a37d09c791d98472868104
Ryuugamine <yoerdanatan@gmail.com>
2024-04-19 13:35:53 +0700

    fix : change html tag homepage to be more semantic

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,17 @@
-"use client";
 import ThemeController from "@/components/ThemeController";
 
 export default function Home() {
   return (
     <main className="max-w-full w-full flex justify-center items-center">
-      <section className="max-w-xl min-h-screen flex-col w-full flex justify-center items-center p-6 md:p-4">
-        <div className="flex w-full items-center justify-between">
+      <article className="max-w-xl min-h-screen flex-col w-full flex justify-center items-center p-6 md:p-4">
+        <section className="flex w-full items-center justify-between">
           <h1 className="text-center font-bold text-3xl md:text-5xl tracking-tight">
             Arukaifu
           </h1>
           <div className="">
             <ThemeController w="2" h="2" />
           </div>
-        </div>
+        </section>
 
         <section className="mt-3 md:mt-4 leading-6 text-base">
           <p>
@@ -26,7 +25,7 @@ export default function Home() {
             <button className="btn">Login</button>
           </a>
         </div>
-      </section>
+      </article>
     </main>
   );
 }
