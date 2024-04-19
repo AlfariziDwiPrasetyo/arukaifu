a7d09590a6f6aa1f29b5d3d7821978a3452d7b8c
Ryuugamine <yoerdanatan@gmail.com>
2024-04-19 14:04:22 +0700

    style : styling card component

--- app/archive-list/page.tsx
+++ app/archive-list/page.tsx
@@ -7,7 +7,7 @@ function page() {
     <main className="max-w-full w-full flex flex-col justify-center items-center">
       <section className="flex items-center flex-between w-full p-6">
         <div className="w-full">
-          <h1 className="text-3xl md:3xl">Note</h1>
+          <h1 className="text-3xl md:3xl">Notes</h1>
         </div>
         <div>
           <button className="">
@@ -16,7 +16,7 @@ function page() {
         </div>
       </section>
 
-      <article className="grid grid-cols-2 md:grid-cols-3 gap-4 p-6">
+      <article className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4 p-6">
         <Card />
         <Card />
         <Card />
--- components/Card.tsx
+++ components/Card.tsx
@@ -4,10 +4,12 @@ import { MdDelete } from "react-icons/md";
 
 function Card() {
   return (
-    <div className="card w-52 md:w-96 bg-base-100 shadow-xl">
-      <div className="card-body">
+    <div className="card w-52 md:w-96 text-accent-content bg-accent">
+      <div className="card-body text-pretty">
         <h2 className="card-title">Card title!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <p className="leading-5">
+          If a dog chews shoes whose shoes does he choose?
+        </p>
         <div className="flex card-actions justify-end">
           <button>
             <FaEdit />
