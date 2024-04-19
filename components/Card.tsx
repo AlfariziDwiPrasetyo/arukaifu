9af2aacd7ce956f68c5732d819b4f2f1e41a1fd7
Ryuugamine <yoerdanatan@gmail.com>
2024-04-19 14:13:37 +0700

    style : add line-clamp for description in card

--- components/Card.tsx
+++ components/Card.tsx
@@ -6,9 +6,12 @@ function Card() {
   return (
     <div className="card w-52 md:w-96 text-accent-content bg-accent">
       <div className="card-body text-pretty">
-        <h2 className="card-title">Card title!</h2>
-        <p className="leading-5">
-          If a dog chews shoes whose shoes does he choose?
+        <h2 className="card-title tracking-tight">Card title!</h2>
+        <p className="leading-5 line-clamp-3 tracking-thight">
+          If a dog chews shoes whose shoes does he choose? If a dog chews shoes
+          whose shoes does he choose? If a dog chews shoes whose shoes does he
+          choose? If a dog chews shoes whose shoes does he choose? If a dog
+          chews shoes whose shoes does he choose?
         </p>
         <div className="flex card-actions justify-end">
           <button>
