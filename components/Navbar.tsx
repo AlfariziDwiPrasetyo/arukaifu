6130a08b4580a59e6e998bfe1f024c9307183bc3
Ryuugamine <yoerdanatan@gmail.com>
2024-04-20 17:43:31 +0700

    chore : make logo navbar link to homepage

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import ThemeController from "./ThemeController";
 import { FiLogOut } from "react-icons/fi";
+import Link from "next/link";
 
 function Navbar() {
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
         <h1 className="text-2xl tracking-tight pl-5">
-          <b className="underline decoration-indigo-500">Aru</b>
-          <b>kaifu</b>
+          <Link href={"/archive"}>
+            <b className="underline decoration-indigo-500">Aru</b>
+            <b>kaifu</b>
+          </Link>
         </h1>
       </div>
       <div className="flex-none p-3">
