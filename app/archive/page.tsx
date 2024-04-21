38629287e573da54559d022e57194ec8a69eede8
Ryuugamine <yoerdanatan@gmail.com>
2024-04-21 11:22:02 +0700

    style : restyling the card and make it more responsive

--- app/archive/page.tsx
+++ app/archive/page.tsx
@@ -41,7 +41,7 @@ async function page() {
         </div>
       </section>
 
-      <article className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4 p-6">
+      <article className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-4 md:p-6 lg:p-8">
         {notes?.map((note) => (
           <Card
             key={note.id}
